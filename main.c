3689e434f1fa274b9bab6efc63b47cfced135b93
AHN-DO-EUN <qwer424@sju.ac.kr>
2024-07-12 12:07:40 +0900

    hw1-2

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-
-
 /* Standard includes. */
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,10 +9,14 @@
 #include "task.h"
 
 /* Local includes. */
-#include "console.h" // posix 환경에서 console을 사용하기 위한 헤더파일. printf 대신 사용
+#include "console.h"
 
 // Simulation of the CPU hardware sleeping mode
 // Idle task hook, 지우면 안됨
+
+
+TaskHandle_t task3;
+
 void vApplicationIdleHook( void )
 {
     usleep( 15000 );
@@ -23,33 +25,49 @@ void vApplicationIdleHook( void )
 // Task 1 정의
 void vTask1( void *pvParameters )
 {
-	const char *pcTaskName = "Task 1: 1 sec period\r\n";
+    const char *pcTaskName = "zzzzzzz\r\n";
 
-	for( ;; )
-	{
-		console_print( pcTaskName );
+    for( ;; )
+    {
+        console_print( pcTaskName );
         vTaskDelay( 1000 );
-	}
+    }
 }
+
 // Task 2 정의
 void vTask2( void *pvParameters )
 {
-	const char *pcTaskName = "Task 2: 2 sec period\r\n";
+    const char *pcTaskName = "알람! 알람! 알람!\r\n";
 
-	for( ;; )
-	{
-		console_print( pcTaskName );
-        vTaskDelay( 2000 );
-	}
+    for( ;; )
+    {
+        console_print( pcTaskName );
+		vTaskDelay( 5000 ); // 5초 delay
+        vTaskResume( task3 ); // Task 3 resume
+    }
 }
+
+//Task 3 정의
+void vTask3( void *pvParameters )
+{
+    const char *pcTaskName = "5분만..\r\n";
+
+    for( ;; )
+    {
+        console_print( pcTaskName );
+        vTaskSuspend( task3 ); // Task 3 suspend
+    }
+}
+
 
 int main( void )
 {
-    console_init(); 
+    console_init();
 
-	xTaskCreate( vTask1, "Task 1", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
-	xTaskCreate( vTask2, "Task 2", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
-    
-	vTaskStartScheduler();
-	for( ;; );
+    xTaskCreate( vTask1, "Task 1", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
+    xTaskCreate( vTask2, "Task 2", configMINIMAL_STACK_SIZE, NULL, 3, NULL );
+    xTaskCreate( vTask3, "Task 3", configMINIMAL_STACK_SIZE, NULL, 2, &task3 );
+
+    vTaskStartScheduler();
+    for( ;; );
 }
